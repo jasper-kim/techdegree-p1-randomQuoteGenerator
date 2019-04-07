2cbc14e24e46654f3407444f7771219272a20242
Jasper Kim <yjikim0219@gmail.com>
2019-04-06 23:39:37 -0700

    Added more features
    Add more properties to the quote object.
    Add a new statement to change background color randomly.
    Add a new statement for auto-refresh the quote.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // Create the 'quotoes' array and assign sevelar quotes
+// To exceed expactation: add an additional property 'category'
 let quotes = [];
 quotes = [
   {
     quote: 'One more thing...',
-    source: 'Steve Jobs'
+    source: 'Steve Jobs',
+    category: 'business'
   },
   {
     quote: 'May the Force be with you.',
@@ -19,16 +21,22 @@ quotes = [
   },
   {
     quote: 'We are shaped by our thoughts; we become what we think. When the mind is pure, joy follows like a shadow that never leaves.',
-    source: 'Gautama Buddha'
+    source: 'Gautama Buddha',
+    category: 'religion'
   },
   {
     quote: 'Think like a queen. A queen is not afraid to fail. Failure is another stepping-stone to greatness.',
-    source: 'Oprah Winfrey'
+    source: 'Oprah Winfrey',
+    category: 'success'
   }
 ];
 
 
-//Create `getRandomQuote` function to return a 'quote' object from quotes array.
+// To exceed expactation: create the 'colors' array to contain a series of background colors.
+let colors = ['#36b55c', '#FB5012', '#3D348B', '#CBBAED', '#E9DF00', '#254E70'];
+
+
+// Create `getRandomQuote` function to return a quote object from quotes array.
 function getRandomQuote () {
   let randomQuote;
   randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
@@ -36,7 +44,8 @@ function getRandomQuote () {
 }
 
 
-//Create the `printQuote` function to return the HTML code that contains values of the quote object created by the function getRandomQuote.
+// Create the `printQuote` function to return the HTML code that contains values of the quote object created by the function getRandomQuote.
+// To exceed expactation: add a conditional statement to display 'category' property.
 function printQuote () {
   let randomeQuote = getRandomQuote();
   let html = '';
@@ -48,10 +57,30 @@ function printQuote () {
   if(randomeQuote.year){
     html += '<span class="year">' + randomeQuote.year + '</span>';
   }
+  if(randomeQuote.category){
+    html += '<span class="category">' + randomeQuote.category + '</span>';
+  }
   html += '</p>';
   document.getElementById('quote-box').innerHTML = html;
 }
 
 
+//To exceed expactation: create the 'changeBgColor' function to change background color randomly.
+function changeBgColor () {
+  let bgColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = bgColor;
+}
+
+
 //Create the even listener to call the 'printQuote' function when clicking the "Show another quote" button in index.html.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+//To exceed expactation: call the 'changeBgColor' function when clicking the "Show another quote" button in index.html.
+document.getElementById('loadQuote').addEventListener("click", changeBgColor, false);
+
+//To exceed expactation: add the 'setInterval' method to show a new quote every 10 seconds.
+setInterval(function(){
+  printQuote();
+  changeBgColor();
+}, 10000);
